f057539e0494dd1ae8719bb21f5f83b12e833aa4
Joeri Smissaert <smissaertj@gmail.com>
2022-06-08 22:26:35 +0400

    Objects: Exercise 1

---
+++ Week_3_JavaScript/Day_3_Conditionals/ex_04/script.js
@@ -0,0 +1,29 @@
+/*
+Create a structured html file linked to a JS file
+
+1. Create an object that has properties "username" and "password". Fill those values in with strings.
+2. Create an array which contains the object you have made above and name the array "database".
+3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+ */
+
+let user = {
+  username: 'joeri',
+  password: 'mypassword'
+}
+
+let database = [user]
+
+let newsfeed = [
+  {
+    username: '',
+    timeline: ''
+  },
+  {
+    username: '',
+    timeline: ''
+  },
+  {
+    username: '',
+    timeline: ''
+  }
+]
