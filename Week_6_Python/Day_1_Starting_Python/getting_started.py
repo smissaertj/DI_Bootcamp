ceab583fdc546ed7124a1711aca4d1541ab416e8
Joeri Smissaert <smissaertj@gmail.com>
2022-07-02 20:49:34 +0400

    Added Hackathon 1

---
+++ Week_6_Python/Day_1_Starting_Python/getting_started.py
@@ -0,0 +1,16 @@
+print('hello world')
+print(5 + 5)
+print((5).__add__(5))
+print('damien'.title())
+print(5/4)
+print(5//4) # Floor division
+print(5 % 4)
+print(type('1'))
+print(type(1))
+print(type(False))
+print(type('False'))
+print(type(1.25))
+
+print('hello world'.capitalize())
+
+
