bd2b3b81d9024dacdf8180090467bbc93992b6cb
Joeri Smissaert <smissaertj@gmail.com>
2022-08-05 19:25:38 +0400

    Day 5 - MiniProject - AnagramChecker

---
+++ Week_10_Modules/Day_5_MiniProject/AnagramChecker/anagrams.py
@@ -0,0 +1,45 @@
+from main import AnagramChecker
+from string import ascii_letters
+
+def menu():
+    """ Show UI """
+
+    while True:
+        print('(I)nput word')
+        print('(E)xit')
+        user_choice = input('Choose a menu option above: ').lower()
+
+        if user_choice == 'i':
+            while True:
+                user_input = input('Please enter one word: ')
+                invalid_chars = [char for char in user_input if char not in ascii_letters]
+
+                if len(user_input.split(' ')) > 1:
+                    # The user provided more than 1 word
+                    print('More than one word was provided.')
+
+                if invalid_chars:
+                    print('Only input letters. No numbers or special characters')
+
+                else:
+                    user_input.strip()
+                    result = AnagramChecker()
+                    is_valid = result.is_valid_word(user_input)
+                    anagrams = result.get_anagrams(user_input)
+
+                    if is_valid:
+                        print(f'{user_input} is a valid English word.')
+                        print(f'Anagrams for your word: {", ".join(anagrams)}')
+                    else:
+                        print(f'{user_input} is not a valid English word.')
+
+                    break
+
+        if user_choice == 'e':
+            break
+
+
+
+if __name__ == '__main__':
+    """ Application entrypoint """
+    menu()
---
+++ Week_10_Modules/Day_5_MiniProject/AnagramChecker/main.py
@@ -0,0 +1,39 @@
+class AnagramChecker():
+    def __init__(self):
+        self.word_list = None
+        with open('sowpods.txt', mode='r') as file:
+            content = file.readlines()
+            self.word_list = [word.lower().strip() for word in content]
+
+
+    def is_valid_word(self, word):
+        """ check if a given word is a valid word """
+        return True if word.lower() in self.word_list else False
+
+    def get_anagrams(self, user_word):
+        """ find all anagrams for the given word """
+        anagrams = []
+        for word in self.word_list:
+            if self.is_anagram(word, user_word.lower()) and word != user_word.lower():
+                anagrams.append(word)
+        return anagrams if len(anagrams) > 0 else None
+
+    @staticmethod
+    def is_anagram(word1, word2):
+        """ return True if both words are the same length and contain the same letters """
+
+        # An anagram is always the same length
+        if len(word1) == len(word2):
+            for letter in word1.lower():
+                # Check if all the letters in word1 are also in word 2
+                if letter not in word2.lower():
+                    return False # as soon as we have 1 letter that is not in word2, we do not have an anagram
+
+                # Inverse check: For each letter we have in word1, check if all the letters in word2 are in word1.
+                for letter in word2.lower():
+                    if letter not in word1.lower():
+                        return False
+            else:
+                return True # If the for loop never returned False (and break the loop), then we have an Anagram.
+        else:
+            return False
