658780a069a4eb9c0eb0e14a8a01465f816b9b33
Joeri Smissaert <smissaertj@gmail.com>
2022-08-19 10:12:16 +0400

    Day 4 - ExercisesXP+

--- Week_12_Flask/Day_04_Exercises/ExercisesXPPlus/xp_plus.sql
+++ Week_12_Flask/Day_04_Exercises/ExercisesXPPlus/xp_plus.sql
@@ -1,16 +1,16 @@
 -- Database: bootcamp
 
-DROP DATABASE IF EXISTS bootcamp;
+-- DROP DATABASE IF EXISTS bootcamp;
 
-CREATE DATABASE bootcamp
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+-- CREATE DATABASE bootcamp
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'en_US.UTF-8'
+--     LC_CTYPE = 'en_US.UTF-8'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
 
 CREATE TABLE students(
 	id SERIAL PRIMARY KEY,
@@ -20,14 +20,14 @@ CREATE TABLE students(
 	
 	
 INSERT INTO
-	students(last_name, first_name, birth_date)
+	students(first_name, last_name, birth_date)
 VALUES
 	('Marc', 'Benichou', '02/11/1998'),
 	('Yoan', 'Cohen', '03/12/2010'),
 	('Lea', 'Benichou', '27/07/1987'),
 	('Amelia', 'Dux', '07/04/1996'),
 	('David', 'Grez', '14/06/2003'),
-	('Omer', 'Simpson', '03/10/1980')
+	('Omer', 'Simpson', '03/10/1980');
 
 
 SELECT * FROM students;
@@ -38,7 +38,7 @@ SELECT first_name, last_name FROM students WHERE first_name ILIKE '%a%';
 SELECT first_name, last_name FROM students WHERE first_name ILIKE 'a%';
 SELECT first_name, last_name FROM students WHERE first_name ILIKE '%a';
 SELECT first_name, last_name FROM students WHERE first_name ILIKE '%a';
-SELECT first_name, last_name FROM students WHERE first_name SIMILAR TO 'a.$';
-SELECT first_name, last_name FROM STUDENTS WHERE id = 1 OR id = 3;
+SELECT first_name, last_name FROM students WHERE first_name LIKE '%a_';
+SELECT first_name, last_name FROM students WHERE id = 1 OR id = 3;
 
 
